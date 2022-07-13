34c7d0839aa2aeff523897036fcefbf01897e6b7
RrodFreitas <rodrigo.orochi@gmail.com>
2022-07-13 12:13:25 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/dto/SaleDTO.java
@@ -0,0 +1,84 @@
+package com.devsuperior.dsmeta.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dsmeta.entities.Sale;
+
+public class SaleDTO {
+
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public SaleDTO() {
+	}
+	
+	public SaleDTO(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+	
+	public SaleDTO(Sale sale) {
+		id = sale.getId();
+		sellerName = sale.getSellerName();
+		visited = sale.getVisited();
+		deals = sale.getDeals();
+		amount = sale.getAmount();
+		date = sale.getDate();
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getSellerName() {
+		return sellerName;
+	}
+	
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	
+	public Integer getVisited() {
+		return visited;
+	}
+	
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+	
+	public Integer getDeals() {
+		return deals;
+	}
+	
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+}
